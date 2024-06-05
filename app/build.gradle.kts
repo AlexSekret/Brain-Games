d02ed12cc3b90427b1535d602153f50b6edb1d62
AlexSekret <alexsekret3@gmail.com>
2024-06-05 21:33:03 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.51.0"
+    application
 }
 
 group = "hexlet.code"
@@ -11,11 +12,14 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.0-M2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
