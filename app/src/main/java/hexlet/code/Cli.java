da520ae09165f82b5f3057eee22397cd2165e2d1
AlexSekret <alexsekret3@gmail.com>
2024-06-06 23:36:08 +0300

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,12 +2,16 @@ package hexlet.code;
 
 import java.util.Scanner;
 
+/* Файл, который мы добавили ранее (src/main/java/hexlet/code/Cli.java), оставьте, как есть,
+ * и не смешивайте с остальным кодом, дальше в проекте он не используется. Он не предназначен для описания логики игр.
+ */
 public class Cli {
-    public static void greet() {
+    public static void start() {
         Scanner sc = new Scanner(System.in);
         System.out.println("""
                 Please enter the game number and press Enter.
                 1 - Greet
+                2 - Even
                 0 - Exit""");
         String userChoice = sc.next();
         System.out.println("Your choice: " + userChoice);
