ead04f4f8cc10385d1c9b9c186ffc61a81da318d
AlexSekret <alexsekret3@gmail.com>
2024-06-06 21:46:33 +0300

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Cli {
     public static void greet() {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
-        String userName = sc.next();
-        System.out.println("Hello, " + userName);
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit""");
+        String userChoice = sc.next();
+        System.out.println("Your choice: " + userChoice);
+        if (userChoice.equals("1")) {
+            System.out.println("Welcome to the Brain Games!\nMay I have your name?");
+            String userName = sc.next();
+            System.out.println("Hello, " + userName);
+        }
     }
 }
