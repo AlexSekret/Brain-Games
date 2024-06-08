42c64fc083b59369a0332e2710dc0392a2e77b39
AlexSekret <alexsekret3@gmail.com>
2024-06-08 19:58:34 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 //Файл App.java предназначен только для отображения игрового меню, выбора и запуска нужной игры
 public class App {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("""
@@ -13,15 +14,20 @@ public class App {
                 0 - Exit""");
         String userChoice = sc.next();
         System.out.println("Your choice: " + userChoice);
-        if (!userChoice.equals("0")) {
-            greet(sc);
+        switch (userChoice) {
+            case "1":
+                greet(sc);
+                break;
+            case "2":
+                EvenOddGame.play();
+                break;
+            default:
+                break;
         }
     }
-
-    private static void greet(Scanner sc) {
+    public static void greet(Scanner sc) {
         System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         String userName = sc.next();
         System.out.println("Hello, " + userName);
     }
-
 }
