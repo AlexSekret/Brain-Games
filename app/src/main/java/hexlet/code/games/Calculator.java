2602021e9134bbca954da3b2de8acedabd33e177
AlexSekret <alexsekret3@gmail.com>
2024-06-13 21:21:19 +0300

    implement Calculator.java class for third game

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -1,4 +1,40 @@
 package hexlet.code.games;
 
 public class Calculator {
+    public static String[] opList = new String[]{"+", "-", "*"};
+    public static String operator;
+    public static int[] numbers = new int[2];
+
+
+    public static String generateQuestion() {
+        operator = Calculator.getRandomOperator();
+        numbers = Calculator.getRandomNumbers();
+        return numbers[0] + " " + operator + " " + numbers[1];
+    }
+
+    public static int generateAnswer(int[] numbers, String operator) {
+        System.out.println(numbers[0] + " " + operator + " " + numbers[1]);
+        switch (operator) {
+            case "+":
+                return numbers[0] + numbers[1];
+            case "-":
+                return numbers[0] - numbers[1];
+            case "*":
+                return numbers[0] * numbers[1];
+            default:
+                return 0;
+        }
+    }
+
+    public static String getRandomOperator() {
+        int rnd = (int) (Math.random() * 3);
+        return opList[rnd];
+    }
+
+    public static int[] getRandomNumbers() {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 101);
+        }
+        return numbers;
+    }
 }
