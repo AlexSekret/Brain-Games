093d0cd75e0ff2d0e624fbd0581d340ea18e608a
AlexSekret <alexsekret3@gmail.com>
2024-06-19 23:42:28 +0300

    refactor Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,241 +1,47 @@
 package hexlet.code;
 
-import hexlet.code.games.ArithmeticProgression;
-import hexlet.code.games.Calculator;
-import hexlet.code.games.EvenOddGame;
-import hexlet.code.games.Gcd;
-import hexlet.code.games.PrimeNumber;
-
 import java.util.Scanner;
 
 public class Engine {
 
-    private static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
     private static String userName = "";
-    private static String userChoice = "";
-    private static String userAnswer;
-    private static String correctAnswer;
     private static int correctAnswersCounter;
     private static int wrongAnswersCount = 0;
 
-    public static String returnUserChoice() {
-        setUserChoice();
-        return userChoice;
-    }
-
-    private static void setUserChoice() {
-        userChoice = sc.next();
-    }
-
-    private static void printUserChoice() {
-        System.out.println("Your choice: " + userChoice);
-    }
-
-    public static void startGreeting() {
-        printUserChoice();
-        defaultGreet();
-        setUserName();
-        System.out.println("Hello, " + userName + "!");
-    }
-
-    private static void defaultGreet() {
-        if (!userChoice.equals("0")) {
-            System.out.println("""
-                    Welcome to the Brain Games!
-                    May I have your name?""");
-        }
-    }
-
-    private static void setUserName() {
+    public static void playGame(String rule, String[] questions, String[] answers, int roundCount) {
+        System.out.println("""
+                Welcome to the Brain Games!
+                May I have your name?""");
         userName = sc.next();
-    }
+        System.out.println("Hello, " + userName + "!");
+        System.out.println(rule); // печатаем правило rule
 
-
-    private static void printCalculatorQuestion() {
-        System.out.println("Question: " + Calculator.getQuestion());
-    }
-
-    private static void printAnswer() {
-        userAnswer = sc.next();
-        System.out.println("Your answer: " + userAnswer);
-    }
-
-    private static boolean checkCalculatorCorrectAnswer() {
-        correctAnswer = Calculator.getAnswer();
-        return userAnswer.equals(correctAnswer);
-    }
-
-    private static void printCalculatorResult() {
-        if (checkCalculatorCorrectAnswer()) {
-            correctAnswersCounter++;
-            printCorrect();
-        } else {
-            wrongAnswersCount++;
-            printWrong();
-        }
-
-    }
-
-    public static void playCalculator(int roundCount) {
-        startGreeting();
-        printCalculatorGreetings();
-        for (int i = 0; i < roundCount; i++) {
+        for (int i = 0; i < roundCount; i++) {      //игровой цикл
             if (wrongAnswersCount == 0) {
-                printCalculatorQuestion();
-                printAnswer();
-                printCalculatorResult();
+                System.out.println("Question: " + questions[i]);
+                String userAnswer = sc.next();
+                System.out.println("Your answer: " + userAnswer);
+                if (userAnswer.equals(answers[i])) {
+                    correctAnswersCounter++;
+                    System.out.println("Correct!");
+                } else {
+                    wrongAnswersCount++;
+                    System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was '" + answers[i] + "'.");
+                    System.out.println("Let's try again, " + userName + "!");
+                }
             }
         }
-        printWinLoose();
-    }
-
-    private static void printWinLoose() {
-        if (correctAnswersCounter == App.MAX_ROUND_COUNT) {
+        if (correctAnswersCounter == roundCount) {
             System.out.println("Congratulations, " + userName + "!");
         }
     }
 
-    public static void playEvenOdd(int roundCount) {
-        startGreeting();
-        printEvenOddGreetings();
-        for (int i = 0; i < roundCount; i++) {
-            if (wrongAnswersCount == 0) {
-                printEvenOddQuestion();
-                printAnswer();
-                printEvenOddResult();
-            }
-        }
-        printWinLoose();
-    }
-
-    private static void printEvenOddResult() {
-        if (checkEvenOddAnswer()) {
-            printCorrect();
-            correctAnswersCounter++;
-        } else {
-            wrongAnswersCount++;
-            printWrong();
-        }
-    }
-
-    private static boolean checkEvenOddAnswer() {
-        correctAnswer = EvenOddGame.getAnswer();
-        return userAnswer.equals(correctAnswer);
-    }
-
-    private static void printEvenOddQuestion() {
-        System.out.println("Question: " + EvenOddGame.getQuestion());
-    }
-
-    public static void playGcd(int roundCount) {
-        startGreeting();
-        printGcdGreetings();
-        for (int i = 0; i < roundCount; i++) {
-            if (wrongAnswersCount == 0) {
-                printGcdQuestion();
-                printAnswer();
-                printGcdResult();
-            }
-        }
-        printWinLoose();
-    }
-
-    private static void printGcdResult() {
-        if (checkGcdAnswer()) {
-            printCorrect();
-            correctAnswersCounter++;
-        } else {
-            wrongAnswersCount++;
-            printWrong();
-        }
-    }
-
-    private static boolean checkGcdAnswer() {
-        correctAnswer = Gcd.getAnswer();
-        return userAnswer.equals(correctAnswer);
-    }
-
-    private static void printGcdQuestion() {
-        System.out.println("Question: " + Gcd.getQuestion());
-    }
-
-
-    public static void playArithmeticProgression(int roundCount) {
-        startGreeting();
-        printAPGreetings();
-        for (int i = 0; i < roundCount; i++) {
-            if (wrongAnswersCount == 0) {
-                printAPQuestion();
-                printAnswer();
-                printAPResult();
-            }
-        }
-        printWinLoose();
-    }
-
-    private static void printAPResult() {
-        if (checkAPAnswer()) {
-            printCorrect();
-            correctAnswersCounter++;
-        } else {
-            wrongAnswersCount++;
-            printWrong();
-        }
-    }
-
-    private static boolean checkAPAnswer() {
-        correctAnswer = ArithmeticProgression.getAnswer();
-        return userAnswer.equals(correctAnswer);
-    }
-
-    private static void printAPQuestion() {
-        System.out.println("Question: " + ArithmeticProgression.getQuestion());
-    }
-
-
-    public static void playPrime(int roundCount) {
-        startGreeting();
-        printPrimeGreetings();
-        for (int i = 0; i < roundCount; i++) {
-            if (wrongAnswersCount == 0) {
-                printPrimeQuestion();
-                printAnswer();
-                printPrimeResult();
-            }
-        }
-        printWinLoose();
-    }
-
-
-    private static void printPrimeQuestion() {
-        System.out.println("Question: " + PrimeNumber.getQuestion());
-    }
-
-    private static void printPrimeResult() {
-        if (checkPrimeAnswer()) {
-            printCorrect();
-            correctAnswersCounter++;
-        } else {
-            wrongAnswersCount++;
-            printWrong();
-        }
-    }
-
-    private static void printCorrect() {
-        System.out.println("Correct!");
-    }
-
-    private static void printWrong() {
-        System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
-        System.out.println("Let's try again, " + userName + "!");
-    }
-
-    private static boolean checkPrimeAnswer() {
-        correctAnswer = PrimeNumber.getAnswer();
-        return userAnswer.equals(correctAnswer);
-    }
-
-    public static void printGameRule(String rule) {
-        System.out.println(rule);
+    public static void playGame() {
+        System.out.println("""
+                Welcome to the Brain Games!
+                May I have your name?""");
+        userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }
