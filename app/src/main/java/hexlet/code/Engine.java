16703f3c9f488e18cbeb39c9a354641f70a19fda
AlexSekret <alexsekret3@gmail.com>
2024-06-22 23:01:22 +0300

    Update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,8 +8,6 @@ public class Engine {
 
     public static void playGame(String rule, String[] questions, String[] answers, int roundCount) {
         Scanner sc = new Scanner(System.in);
-        int correctAnswersCounter = 0;
-        int wrongAnswersCount = 0;
         System.out.println("""
                 Welcome to the Brain Games!
                 May I have your name?""");
@@ -18,24 +16,20 @@ public class Engine {
         System.out.println(rule); // печатаем правило rule
 
         for (int i = 0; i < roundCount; i++) {      //игровой цикл
-            if (wrongAnswersCount == 0) {
-                System.out.println("Question: " + questions[i]);
-                String userAnswer = sc.next();
-                System.out.println("Your answer: " + userAnswer);
-                if (userAnswer.equals(answers[i])) {
-                    correctAnswersCounter++;
-                    System.out.println("Correct!");
-                } else {
-                    wrongAnswersCount++;
-                    System.out.print("'" + userAnswer + "'" + " is wrong answer ;(. ");
-                    System.out.print("Correct answer was '" + answers[i] + "'. ");
-                    System.out.println("Let's try again, " + userName + "!");
-                }
+            System.out.println("Question: " + questions[i]);
+            String userAnswer = sc.next();
+            System.out.println("Your answer: " + userAnswer);
+            if (userAnswer.equals(answers[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.print("'" + userAnswer + "'" + " is wrong answer ;(. ");
+                System.out.print("Correct answer was '" + answers[i] + "'. ");
+                System.out.println("Let's try again, " + userName + "!");
+                break;
             }
+
         }
-        if (correctAnswersCounter == roundCount) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
+        System.out.println("Congratulations, " + userName + "!");
     }
 
     public static void playGame() {
