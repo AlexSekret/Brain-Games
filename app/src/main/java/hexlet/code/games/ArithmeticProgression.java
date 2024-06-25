da6eb7d55cdbbf73b31ff0ed852c358b2bb576f1
AlexSekret <alexsekret3@gmail.com>
2024-06-25 20:55:22 +0300

    refactor ArithmeticProgression.java

--- app/src/main/java/hexlet/code/games/ArithmeticProgression.java
+++ app/src/main/java/hexlet/code/games/ArithmeticProgression.java
@@ -9,39 +9,33 @@ public class ArithmeticProgression {
     private static final int NUMBER_RANGE = 100;
 
     public static void start() {
-        //генерируем игровые данные
         String gameRule = "What number is missing in the progression?";
-        String[][] gameData = new String[Engine.MAX_ROUND_COUNT][Engine.QA_COUNT]; //{{a,b},{c,d},{e,f}} M*N, M=3, N=2
-        //gd[0] - store questions, gd[1] - store answers
+        String[][] gameData = new String[Engine.MAX_ROUND_COUNT][Engine.QA_COUNT];
         for (String[] gd : gameData) {
-            int progressionLength = MIN_LENGTH + Util.getRandomNumber(MIN_LENGTH); //длина прогрессии от 5 до 10
+            int progressionLength = MIN_LENGTH + Util.getRandomNumber(MIN_LENGTH);
             int position = Util.getRandomNumber(progressionLength);
             int step = 1 + Util.getRandomNumber(MAX_STEP_RANGE);
-            int[] progression = generateProgression(step, progressionLength);
-            gd[0] = generateQuestion(progression, position);
+            int first = Util.getRandomNumber(NUMBER_RANGE);
+            int[] progression = generateProgression(step, progressionLength, first);
+            StringBuilder question = new StringBuilder();
+            for (int i = 0; i < progression.length; i++) {
+                if (i != position) {
+                    question.append(progression[i]).append(" ");
+                } else {
+                    question.append("..").append(" ");
+                }
+            }
+            gd[0] = question.toString().trim();
             gd[1] = String.valueOf(progression[position]);
         }
         Engine.playGame(gameRule, gameData);
 
     }
 
-    private static String generateQuestion(int[] progression, int position) {
-        StringBuilder question = new StringBuilder();
-        for (int i = 0; i < position; i++) {
-            question.append(progression[i]).append(" ");
-        }
-        question.append("..").append(" ");
-        for (int i = position + 1; i < progression.length; i++) {
-            question.append(progression[i]).append(" ");
-        }
-        return question.toString();
-    }
-
-    private static int[] generateProgression(int step, int progressionLength) {
+    private static int[] generateProgression(int step, int progressionLength, int first) {
         int[] progression = new int[progressionLength];
-        progression[0] = Util.getRandomNumber(NUMBER_RANGE);
-        for (int i = 1; i < progression.length; i++) {
-            progression[i] = progression[i - 1] + step;
+        for (int i = 0; i < progression.length; i++) {
+            progression[i] = first + step * i;
         }
         return progression;
     }
